d5b1bcee8c0bf2ee9aaa5c1482876ad23bd7f37d
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-01-02 10:47:20 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (97.51%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,19 +1,25 @@
 class Solution {
+    public boolean isCapital(char ch) {
+        return (ch >= 'A' && ch <= 'Z') ? true : false;
+    }
+    public boolean isNonCapital(char ch) {
+        return (ch >= 'a' && ch <= 'z') ? true : false;
+    }
     public boolean detectCapitalUse(String word) {
         int capitals = 0;
         int non_capitals = 0;
         final int n = word.length();
         for(int i = 0; i < n; i++) {
             char ch = word.charAt(i);
-            if(ch >= 'a' && ch <= 'z') {
+            if(isNonCapital(ch)) {
                 non_capitals++;
-            } else if(ch >= 'A' && ch <= 'Z') {
+            } else if(isCapital(ch)) {
                 capitals++;
             }
         }
         if(capitals == n) return true;
         else if(non_capitals == n) return true;
-        else if(capitals == 1 && (non_capitals + capitals) == n && word.charAt(0) >= 'A' && word.charAt(0) <= 'Z') return true;
+        else if(capitals == 1 && (non_capitals + capitals) == n && isCapital(word.charAt(0))) return true;
         
         return false;
     }
