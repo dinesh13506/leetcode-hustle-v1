074ebf0d2e55b069cc2fa10820ea3ecffef56f45
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-01-14 22:34:54 +0530

    Time: 7 ms (36.93%), Space: 42.7 MB (27.84%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
@@ -1,20 +1,28 @@
 class Solution {
-        public void dfs(char u,HashSet<Character> visited, HashMap<Character, ArrayList<Character>> adjList, ArrayList<Character> component) {
+    HashMap<Character, ArrayList<Character>> adjList;
+    HashSet<Character> visited;
+    ArrayList<Character> verticies;
+    HashMap<Character, Character> equivalent;
+        
+    public void dfs(char u, ArrayList<Character> component) {
         if(visited.contains(u)) return;
         visited.add(u);
         component.add(u);
         if(adjList.containsKey(u)) {
             for(char v : adjList.get(u)) {
-                dfs(v, visited, adjList, component);
+                dfs(v, component);
             }
         }
     }
 
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        HashMap<Character, ArrayList<Character>> adjList = new HashMap<>();
-        int n1 = s1.length(), n2 = s2.length();
-        ArrayList<Character> verticies = new ArrayList<>();
-        for(int i = 0; i < n1; i++) {
+        int n = s1.length();
+        adjList = new HashMap<>();
+        visited = new HashSet<>();
+        verticies = new ArrayList<>();
+        equivalent = new HashMap<>();
+        
+        for(int i = 0; i < n; i++) {
             char u = s1.charAt(i), v = s2.charAt(i);
             verticies.add(u);
             verticies.add(v);
@@ -24,17 +32,14 @@ class Solution {
             if(adjList.containsKey(v) == false) {
                 adjList.put(v, new ArrayList<>());
             }
-
             adjList.get(u).add(v);
             adjList.get(v).add(u);
         }
 
-        HashSet<Character> visited = new HashSet<>();
-        HashMap<Character, Character> equivalent = new HashMap<>();
         for(char u : verticies) {
             if(visited.contains(u) == false) {
                 ArrayList<Character> component = new ArrayList<>();
-                dfs(u, visited, adjList,component);
+                dfs(u,component);
                 Collections.sort(component);
                 char start = component.get(0);
                 for(char a : component) {
@@ -43,7 +48,6 @@ class Solution {
             }
         }
         StringBuilder sb = new StringBuilder();
-        //System.out.println(baseStr.length());
         for(int i = 0; i < baseStr.length(); i++) {
             char ch = baseStr.charAt(i);
             if(equivalent.containsKey(ch)) {
@@ -53,7 +57,6 @@ class Solution {
             }
            
         }
-        //System.out.println(sb.length());
         return sb.toString();
     }
 }
