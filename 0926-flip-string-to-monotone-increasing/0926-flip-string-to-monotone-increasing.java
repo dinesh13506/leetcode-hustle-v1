e92fcb6bb91288ac9440487eb252b95576637a69
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-01-17 11:28:05 +0530

    Time: 45 ms (12.88%), Space: 63.6 MB (9.83%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -1,22 +1,24 @@
 class Solution {
+    int[] prefixOne;
+    public int dp(int len, String s) {
+        if(len <= 0) return 0;
+        char ch = s.charAt(len-1);
+        if(ch == '1') {
+            return dp(len-1, s);
+        } else {
+            return Math.min(prefixOne[len-1],1 + dp(len-1,s));
+        }
+    }
+    
     public int minFlipsMonoIncr(String s) {
         int n = s.length();
-        int left1   = 0;
-        int right0  = 0;
-        for(char ch : s.toCharArray()) {
-            if(ch == '0') right0++;
-        }
-        int ans = right0; //full right window
+        prefixOne = new int[n];
+        int sum = 0;
         for(int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if(ch == '0') {
-                right0--;
-            }
-            else {
-                left1++;
-            }
-            ans = Math.min(ans, left1 + right0);
+            if(ch == '1') sum++;
+            prefixOne[i] = sum;
         }
-        return ans;
+        return dp(n,s);
     }
 }
