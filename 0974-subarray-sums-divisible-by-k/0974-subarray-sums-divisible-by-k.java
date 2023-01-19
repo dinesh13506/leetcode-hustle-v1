d681ec732c9519ff9863967574d0288a6103ff94
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-01-19 14:21:10 +0530

    Time: 20 ms (72.47%), Space: 47.7 MB (54.88%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,16 +1,17 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
         int n = nums.length;
+        //store freq of remainder of sum % k
         HashMap<Integer, Integer> freqMap = new HashMap<Integer, Integer>();
-        freqMap.put(0,1);
+        freqMap.put(0,1); //there exists empty array with sum = 0, sum % k = 0;
         int sum = 0;
         int count = 0;
         for(int i = 0; i < n; i++) {
             sum += nums[i];
-            int remainder = sum % k;
-            if(remainder < 0) remainder += k; 
-            count += freqMap.getOrDefault(remainder,0);
-             freqMap.put(remainder,freqMap.getOrDefault(remainder,0) + 1);
+            int remainder = (sum % k);
+            if(remainder < 0) remainder += k; //make remainder positive
+            count += freqMap.getOrDefault(remainder,0); //add subarrays at a distance  = multiple of k 
+            freqMap.put(remainder,freqMap.getOrDefault(remainder,0) + 1);
         }
         return count;
     }
