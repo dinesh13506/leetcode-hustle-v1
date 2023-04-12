ba90c1b9df52ad42bbe2935357a179139c250947
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-04-12 21:13:27 +0530

    Time: 71 ms (46.49%), Space: 44 MB (74.21%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    
+    let components = path.split("/")
+    let stack = []
+    
+    for( let component of components ) {
+        if(component === ""  || component === ".") {
+            continue
+        }
+        if(component === "..") {
+            if(stack.length) {
+                stack.pop()
+            }
+        } else {
+            stack.push(component)
+        }
+    }
+    return stack.length > 0 ? ("/" + stack.join("/") ) : "/"
+};
