e85ef046cfd682c00f4c48e4eef4b7d7161163b2
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-05-03 20:51:54 +0530

    Time: 6 ms (88.07%), Space: 42.7 MB (95.95%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,28 +1,37 @@
 class Solution {
     public String frequencySort(String s) {
         /*
-        System.out.println('0' - 0);
-        System.out.println('9' - 0);
-        System.out.println('A' - 0);
-        System.out.println('z' - 0);
+        ascii value ranges
+        0 - 9 => 48 - 57
+        A-Z => 65 - 90
+        a - z => 97 - 122
+        so count array can have size => 122(max) - 48(min) + 1
+        0 - 9 will fit in  0 - 9 index range
+        A - Z will fit in 17 - 42 index range
+        a - z will fit in 49 - 74 index range
         */
+        int len = 122 - 48 + 1; // 75
         
-        int len = 122 - 48 + 1;
-        int[] count = new int[len];
+        int[] count = new int[len]; //define count array
+        
+        //init counts to zero 
         for(int i = 0; i < len; i++) {
             count[i] = 0;
         }
         
+        // calculate freq of every element
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             int idx = ch - 48;
             count[idx]++;
         }
         
+        //define priority queue / heap
         PriorityQueue<int[] > pq = new PriorityQueue<int[]> ((a,b) -> {
              return b[1] - a[1];
         });
         
+        //add elements which are present and have freq > 0
         for(int i = 0; i < len; i++) {
             if(count[i] != 0) {
                 int[] arr = new int[2];
@@ -32,6 +41,8 @@ class Solution {
             }
         }
         
+        
+        //poll frequent element from heap and generate op
         StringBuilder sb = new StringBuilder("");
         while(!pq.isEmpty()) {
             int[] arr = pq.poll();
