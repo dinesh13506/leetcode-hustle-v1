cfe409a15ef3a6aa2b1d8d73a124f44353028ea7
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-06-05 21:46:55 +0530

    Time: 60 ms (74.40%), Space: 44.3 MB (36.72%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -9,12 +9,10 @@ var checkStraightLine = function(coordinates) {
         if(a[0] == b[0]) return a[1] - b[1];
         return a[0] - b[0];
     });
-    //console.log(coordinates);
 
     let set = new Set();
     for(let i = 1; i < n; i++) {
             let slope = Math.abs(coordinates[i][1] - coordinates[i-1][1]) / Math.abs(coordinates[i][0] - coordinates[i-1][0]);
-            //console.log(slope);
             set.add(slope);
             if(set.size > 1) return false;
     }
