83c21e94dd213274472291cb4392bea923fe0422
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-06-07 22:46:20 +0530

    Time: 51 ms (89.58%), Space: 41.4 MB (87.50%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var minFlips = function(a, b, c) {
+    
+    let count = 0;
+    let carry = 0;
+    while(a > 0 || b > 0 || c > 0) {
+        let last_a = a & 1;
+        let last_b = b & 1;
+        let last_c = c & 1;
+        //console.log(last_a, last_b, last_c);
+        if( (last_a | last_b | carry) != last_c) {
+            if(last_c == 1) {
+                if( last_a == 0 && last_b == 0)  {
+                    count++;
+                }
+            } else {
+                if( last_a == 1 && last_b == 0)  {
+                    count++;
+                } else if( last_a == 0 && last_b == 1)  {
+                    count++;
+                } else if( last_a == 1 && last_b == 1)  {
+                    count = count + 2;
+                }
+            }
+        } else {
+            if( last_a == 1 && last_b == 1)  {
+                carry = 0;
+            }
+        }
+        a = a >> 1;
+        b = b >> 1;
+        c = c >> 1;
+    }
+    return count;
+    
+};
