bc81a8e3436e6a75f916eb3e7d6ce09f40ce93fe
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-06-08 22:15:53 +0530

    Time: 58 ms (78.26%), Space: 43.9 MB (74.38%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegativesBrute = function(grid) {
+    
+    let count = 0
+    let m = grid.length, n = grid[0].length
+    for(let i = 0; i < m; i++) {
+        for(let j = n-1; j >=0; j--) {
+            if(grid[i][j] >= 0) {
+                break
+            } else {
+                count++
+            }
+        }
+    }
+    return count
+};
+
+
+var countNegatives = function(grid) {
+    
+    let count = 0
+    let m = grid.length, n = grid[0].length
+    for(let i = 0; i < m; i++) {
+        if(grid[i][0] < 0 ) {
+            count += n
+            continue
+        }
+        if(grid[i][n-1] >= 0) {
+            continue
+        }
+        
+        let start = 0, end = n-1
+        while(start <= end) {
+            let mid = parseInt((start + end)/2)
+            if(grid[i][mid] >= 0) {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+        count += (n - start)
+    }
+    return count
+};
