f3506f929bc2e9b72f5911e4a5a83b64c400b7b7
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-08-29 09:48:13 +0530

    Time: 93 ms (17.65%), Space: 51.7 MB (25.49%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} customers
+ * @return {number}
+ */
+var bestClosingTime = function(customers) {
+    
+    let str = customers.split('');
+    const n = str.length;
+    let leftNs = new Array(n+1);
+    let rightYs = new Array(n+1);
+    
+    let count = 0;
+    for(let i = 0; i <= n ; i++) {
+        leftNs[i] = count;
+        if(i < n) {
+            count = str[i] == 'N' ? count + 1 : count;
+        }
+    }
+    //console.log(leftNs);
+    count = 0;
+    for(let i = n; i >= 0; i--) {
+        rightYs[i] = count;
+        if(i < n && i >= 0) {
+            count = str[i] == 'Y' ? count + 1 : count;
+        }
+    }
+    //console.log(rightYs);
+    let ans = Number.MAX_VALUE;
+    let penality = n + 1;
+    for(let i = 0; i <= n; i++) {
+        let p = 0;
+        p = p + leftNs[i];
+        p = p + rightYs[i];
+        if(i >= 0 && i < n) {
+            p = str[i] == 'Y' ? p + 1 : p;
+        }
+        
+        if( p < penality) {
+            penality = p;
+            ans = i;
+        }
+    }
+    return ans;
+    
+};
