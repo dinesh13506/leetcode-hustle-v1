a2ef875d781fcc5062d7105cf8a1f2aceb1d9472
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-11-14 12:57:31 +0530

    Time: 150 ms (22.67%), Space: 73.9 MB (57.33%) - LeetHub

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.js
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.js
@@ -4,15 +4,13 @@
  */
 var findPrefixScore = function(nums) {
     const n = nums.length;
-    let conver = new Array(n);
     let ans = new Array(n);
     let max = 0;
     let sum = 0;
     for(let i = 0; i < n; i++)  {
         ans[i] = 0;
         max = max < nums[i] ? nums[i] : max;
-        conver[i] = max + nums[i];
-        sum += conver[i];
+        sum += (max + nums[i]);
         ans[i] = sum;
     }
     return ans;
