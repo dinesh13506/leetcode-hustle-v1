6ede8839303e4b0bdbedc1f4ca11b11aca35a177
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-11-15 21:01:11 +0530

    Time: 54 ms (79.73%), Space: 44.9 MB (24.73%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let curr = init;
+    let increment = () => {
+        curr++;
+        return curr;
+    }
+    let decrement = () => {
+        curr--;
+        return curr;
+    }
+    let reset = () => {
+        curr = init;
+        return curr;
+    }
+    
+    return {
+       increment,  decrement, reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
