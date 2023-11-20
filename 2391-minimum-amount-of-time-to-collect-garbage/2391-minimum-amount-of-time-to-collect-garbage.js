f93cd12fa64a0ac0a68e70ee8d487b2c7462b204
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-11-20 21:30:34 +0530

    Time: 231 ms (13.10%), Space: 73.1 MB (12.78%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.js
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.js
@@ -24,29 +24,25 @@ var garbageCollection = function(garbage, travel) {
                     mrange[0] = i;
                     mrange[1] = i;
                 } else {
-                     //mrange[0] = Math.min(mrange[0], i);
-                     mrange[1] = Math.max(mrange[1], i);
+                     mrange[1] = i;
                 }
             } else if(ch == 'P') {
                 if(prange[0] == null || prange[1] == null) {
                     prange[0] = i;
                     prange[1] = i;
                 } else {
-                     //prange[0] = Math.min(prange[0], i);
-                     prange[1] = Math.max(prange[1], i);
+                     prange[1] = i;
                 }
             } else if(ch == 'G') {
                 if(grange[0] == null || grange[1] == null) {
                     grange[0] = i;
                     grange[1] = i;
                 } else {
-                     //grange[0] = Math.min(grange[0], i);
-                     grange[1] = Math.max(grange[1], i);
+                     grange[1] = i;
                 }
             }
         }
     }
-    //console.log(map, mrange, prange, grange);
     let ops = 0;
     for(let ch of map.keys()) {
         ops+= map.get(ch);
