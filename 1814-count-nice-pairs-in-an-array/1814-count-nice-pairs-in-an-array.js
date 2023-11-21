fc61e8c27a5317c19c5274ed433e33686a39b0c6
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-11-21 17:29:58 +0530

    Time: 149 ms (76.00%), Space: 60.6 MB (72.00%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const mod = Math.pow(10, 9) + 7;
+var countNicePairs = function(nums) {
+    let pairs = 0;
+    let map = new Map();
+    let reverse = (num) => {
+        let ans = 0;
+        while(num > 0) {
+            let last = num % 10;
+            ans = (ans * 10) + last;
+            num = parseInt(num / 10);
+        }
+        return ans;
+    }
+    
+    for(let num of nums) {
+        let diff = (num - reverse(num));
+        if(map.has(diff)) {
+            map.set(diff, map.get(diff) + 1);
+        } else {
+            map.set(diff,  1);
+        }
+    }
+    //console.log(map);
+    
+    for(let key of map.keys()) {
+        let n = map.get(key);
+        pairs = (pairs + (n * (n-1)/ 2)) % mod;
+    }
+    return pairs;
+    
+};
