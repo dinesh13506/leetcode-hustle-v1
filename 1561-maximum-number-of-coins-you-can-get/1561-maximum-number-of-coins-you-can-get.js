4ad3867c3d88356e25c2395e8f80e1784bfad3df
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-11-24 21:27:56 +0530

    Time: 157 ms (97.67%), Space: 52 MB (62.79%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var maxCoins = function(piles) {
-    
+    const n = piles.length;
     let p1 = 0;
     let p2 = 1;
-    let p3 = piles.length - 1;
-    const n = piles.length;
+    let p3 = n - 1;
+    let ans = 0;
     piles.sort((a,b) => {
          return b - a;
     });
-    //console.log(piles);
-    let ans = 0;
+    
     while(p1 < n && p2 < n && p3 > p2 && p3 >= 0) {
-        //console.log(piles[p2])
         ans += piles[p2];
         p1 = p2 + 1;
         p2 = p1 + 1;
