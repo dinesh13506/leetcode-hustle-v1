42c22c5512c2fa7f6746057f66e84012815af05a
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-11-29 10:19:01 +0530

    Time: 56 ms (58.79%), Space: 42.2 MB (71.45%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    let count = 0
+    let count = 0;
     while(n!= 0) {
-        let bit = n &1
+        let bit = n & 1; //get last bit
         if(bit == 1) {
-            count++
+            count++;
         }
-        n = n >>> 1
+        n = n >>> 1; //right shift by 1 but sign should zero copied
     }
-    return count
+    return count;
 };
 
