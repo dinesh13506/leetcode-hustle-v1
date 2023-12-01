06bf60d44d15db02101e4a614030ab1f3510d3af
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-12-01 22:11:27 +0530

    Time: 56 ms (25.98%), Space: 42.3 MB (30.51%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -5,5 +5,29 @@
  */
 var arrayStringsAreEqual = function(word1, word2) {
     
-    return word1.join("") == word2.join("");
+    let p1 = 0;
+    let p2 = 0;
+    let i = 0; 
+    let j = 0;
+    const n1 = word1.length;
+    const n2 = word2.length;
+    while(p1 < n1 || p2 < n2) {
+        if(i >= word1[p1].length) {
+            p1++;
+            i = 0;
+        }
+        if(j >= word2[p2].length) {
+            p2++;
+            j = 0;
+        }
+        if(p1 >= n1 || p2 >= n2) {
+            break;
+        }
+        if(word1[p1].charAt(i) != word2[p2].charAt(j)) {
+            return false;
+        }
+        i++;
+        j++;
+    }
+    return p1>= n1 && p2>= n2 ? true : false;;
 };
