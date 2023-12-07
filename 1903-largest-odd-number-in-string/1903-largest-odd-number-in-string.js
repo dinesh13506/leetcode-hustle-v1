4351fad41eb0532613a3282ab0bb4a7fa70a430f
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-12-07 13:26:54 +0530

    Time: 129 ms (5.31%), Space: 77.9 MB (5.59%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    let firstOdd = null;
+    let firstEven = null;
+    let lastOdd = null;
+    for(let i = 0; i < num.length; i++) {
+        let d = parseInt(num.charAt(i));
+        if(d & 1) {
+            if(firstOdd == null) {
+                firstOdd = i;
+            }
+            lastOdd = i;
+        } else {
+            if(firstEven == null && d > 0) {
+                firstEven = i;
+            }
+        }
+    }
+    //console.log(firstOdd, firstEven, lastOdd)
+    let str1 = "";
+    if(firstOdd != null && lastOdd != null) {
+       for(let p1 = firstOdd; p1 <= lastOdd; p1++) {
+            str1 += num.charAt(p1);
+        } 
+    }
+    
+    let str2 = "";
+    if(firstEven != null && lastOdd != null) {
+       for(let p1 = firstEven; p1 <= lastOdd; p1++) {
+         str2 += num.charAt(p1);
+       } 
+    }
+    
+    //console.log(str1, str2)
+    let ans = "";
+    if(!str1 && !str2) return ans;
+    else if(str1 && !str2) return str1;
+    else if(!str1 && str2) return str2;
+    return str1.length > str2.length ? str1 : str2;
+    
+};
