51b87492515b26fc4ba0cbddff995f536d20abc2
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-12-21 13:13:17 +0530

    Time: 168 ms (8.45%), Space: 62 MB (69.01%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.js
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.js
@@ -3,14 +3,15 @@
  * @return {number}
  */
 var maxWidthOfVerticalArea = function(points) {
+    let ans = 0;
     
+    const n = points.length;
     points.sort((a,b) => {
         if(a[0] == b[0]) return a[1] - b[1];
         return a[0] - b[0];
     });
-    let ans = 0;
     let prev = points[0];
-    for(let i = 1; i < points.length; i++) {
+    for(let i = 1; i < n; i++) {
         if(points[i][0] > prev[0]) {
             ans = Math.max(ans, points[i][0] - prev[0]);
             prev = points[i];
