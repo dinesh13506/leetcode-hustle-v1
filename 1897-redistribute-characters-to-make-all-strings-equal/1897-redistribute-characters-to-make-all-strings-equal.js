80af51c97530c8564851bd139240e1e65d7b23d7
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2023-12-30 20:10:06 +0530

    Time: 93 ms (9.86%), Space: 44.8 MB (70.42%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
@@ -4,12 +4,13 @@
  */
 var makeEqual = function(words) {
     const n = words.length;
-    let temp = words.join('');
     let count = new Map();
-    for(let ch of temp) {
-        let c = count.get(ch) || 0;
-        c++;
-        count.set(ch, c);
+    for(let i = 0; i < n; i++) {
+        for(let ch of words[i]) {
+            let c = count.get(ch) || 0;
+            c++;
+            count.set(ch, c);
+        }
     }
     for(let ch of count.keys()) {
         if(count.get(ch) % n != 0) {
