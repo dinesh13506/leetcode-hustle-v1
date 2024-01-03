781a04b7ccb794d3376f1373b5edad1fb0ca38c4
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2024-01-03 19:52:51 +0530

    Time: 82 ms (64.71%), Space: 54.7 MB (5.88%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string[]} bank
+ * @return {number}
+ */
+var numberOfBeams = function(bank) {
+    const m = bank.length;
+    const n = bank[0].length;
+    //console.log(m,n)
+    let securityDevices = new Array(m);
+    let prefixSum = new Array(m);
+    prefixSum.fill(0);
+    for(let i = 0; i < m; i++) {
+        securityDevices[i] = [];
+        let deviceCount = 0;
+        for(let j = 0;  j < n; j++) {
+            let ch = bank[i].charAt(j);
+            if(ch == '1') {
+                securityDevices[i].push(j);
+                deviceCount++;
+            }
+        }
+        prefixSum[i] =  i > 0 ? prefixSum[i-1] + deviceCount : deviceCount;
+    }
+    //console.log(securityDevices,prefixSum)
+    let ans = 0;
+    for(let r1 = 0; r1 < securityDevices.length; r1++) {
+        if(securityDevices[r1].length == 0) continue;
+        for(let r2 = r1 + 1; r2 < securityDevices.length; r2++) {
+            if(securityDevices[r2].length == 0) continue;
+            let flag = true;
+            if(r2 - r1 == 1) {
+               ans = ans + (securityDevices[r1].length * securityDevices[r2].length);
+               //continue;
+                break;
+            }
+            else if(prefixSum[r1 + 1] == prefixSum[r2 - 1]) {
+                ans = ans + (securityDevices[r1].length * securityDevices[r2].length);
+                 break;
+            }
+        }
+    }
+    return ans;
+};
