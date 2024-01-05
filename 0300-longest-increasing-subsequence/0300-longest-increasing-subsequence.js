8a226c557917293242f0a382338ec34120fce2c5
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2024-01-05 22:18:36 +0530

    Time: 46 ms (99.32%), Space: 44.7 MB (13.53%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let n = nums.length
+    let temp = []
+    
+    //find index where target is found or just greater value is found
+    let lowerBound = function(target) {
+        let start = 0, end = temp.length - 1, ans = -1
+        while(start <= end) {
+            let mid = parseInt((start + end) / 2)
+            if(temp[mid] === target) {
+                ans = mid
+                break
+            } else if(target < temp[mid]) {
+                ans = mid
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        }
+        return ans
+    }
+    
+    let len = 0
+    temp.push(nums[0])
+    len++
+    for(let i = 1; i < n; i++) {
+        if(nums[i] > temp[temp.length-1]) {
+            temp.push(nums[i])
+            len++
+        } else {
+            let idx = lowerBound(nums[i])
+            temp[idx] = nums[i]
+        }
+    }
+    return len
+};
