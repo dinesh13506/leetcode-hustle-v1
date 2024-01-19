631776aba0eb27154084d34a064d5adea29afc40
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2024-01-19 20:53:37 +0530

    Time: 66 ms (38.85%), Space: 51.7 MB (5.73%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
@@ -3,33 +3,27 @@
  * @return {number}
  */
 var minFallingPathSum = function(matrix) {
-    
-    let m = matrix.length;
-    let n = matrix[0].length;
-    
-    let memo = new Array(m);
-    for(let i = 0; i < m; i++) {
+    let n = matrix.length;
+
+    let memo = new Array(n);
+    for(let i = 0; i < n; i++) {
         memo[i] = new Array(n);
+        memo[i].fill(null);
     }
-    
-    let dp = (i , j) => {
-        if(i < 0 || j < 0 || i >= m || j >= n) return Infinity;
-        if(i == m - 1) {
-            return matrix[i][j];
-        }
-        
-        if(memo[i][j]) return memo[i][j];
-        
-        let a1 = dp(i+1, j-1);
-        let a2 = dp(i+1, j);
-        let a3 = dp(i+1, j+1);
-        memo[i][j] =  matrix[i][j] + Math.min(a1, Math.min(a2, a3));
+    let dp = (i, j) => {
+        if(i < 0 || i >= n || j < 0 || j >=n) return Infinity;
+        if(i == 0) return matrix[i][j];
+        if(memo[i][j] != null) return memo[i][j];
+        let top = matrix[i][j] + dp(i-1,j);
+        let left =  matrix[i][j] + dp(i-1,j-1);
+        let right = matrix[i][j] + dp(i-1,j+1);
+        memo[i][j] =  Math.min(top, Math.min(left, right));
         return memo[i][j];
     }
-    
+
     let ans = Infinity;
     for(let j = 0; j < n; j++) {
-        ans = Math.min(ans, dp(0, j));
+        ans = Math.min(ans, dp(n-1, j));
     }
     return ans;
 };
