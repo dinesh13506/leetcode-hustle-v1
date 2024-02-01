81909a460ef94ee8a969bdb84b232c8e9ec1f5de
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2024-02-01 20:19:58 +0530

    Time: 343 ms (42.86%), Space: 79.9 MB (94.92%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
@@ -4,26 +4,25 @@
  * @return {number[][]}
  */
 var divideArray = function(nums, k) {
-    nums.sort((a, b) => {
+    nums.sort((a,b) => {
         return a - b;
-    })
+    });
+    
     let n = nums.length;
+    let p = 0;
     let ans = [];
-    if(n % 3 != 0) {
-        return ans;
-    }
-    //console.log(nums)
-    for(let i = 0; i < n; i = i + 3) {
-        let d1 = Math.abs(nums[i]-nums[i+1]);
-        let d2 = Math.abs(nums[i + 1]-nums[i + 2]);
-        let d3 = Math.abs(nums[i]-nums[i + 2]);
-        //console.log(d1, d2, d3)
-        if(d1 > k || d2 > k || d3 > k) {
-            //return [];
-            continue;
+    while(p < n && p + 1 < n && p + 2 < n) {
+        let d1 = Math.abs(nums[p]- nums[p+1]);
+        let d2 = Math.abs(nums[p+1]- nums[p+2]);
+        let d3 = Math.abs(nums[p]- nums[p+2]);
+        if(d1 <= k && d2 <= k && d3 <= k) {
+            ans.push([nums[p], nums[p + 1], nums[p+2]]);
+            p = p + 3;
         } else {
-            ans.push([nums[i], nums[i+1], nums[i+2]]);
+            p = p + 1;
         }
     }
-    return ans.length * 3 != nums.length ? [] : ans;
+    //console.log(ans);
+    if(ans.length * 3 == n) return ans;
+    return [];
 };
