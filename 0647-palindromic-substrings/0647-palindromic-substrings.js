db86197de719a0ac6182aeb0171b7dab3330b9b0
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2024-02-10 23:12:54 +0530

    Time: 65 ms (55.73%), Space: 49.3 MB (48.66%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    
+    let n = s.length
+    let result = 0
+    
+    let countPalindromes = function(start, end) {
+        let count = 0
+        while(start >=0 && end < n) {
+            //expand to bigger palindromes
+            if(s[start] == s[end]) {
+                start--
+                end++
+                count++
+            } else {
+                break
+            }
+        }
+        return count
+    }
+    for(let centerIndex = 0; centerIndex < n; centerIndex++) {
+        //odd length palindrome
+        result += countPalindromes(centerIndex,centerIndex)
+        //even length of palindrome
+        result += countPalindromes(centerIndex,centerIndex + 1)
+        
+    }
+    return result
+};
